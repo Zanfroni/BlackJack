a7dcd910d7c16a32d84502a95788d65d1dddb1fb
Guilherme <munaretto.guilherme@gmail.com>
2017-06-13 19:25:12 -0300

    Iniciado o desenvolvimento da classe Blackjack. Criado as referências necessárias (até o momento) para a realização da lógica do jogo

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,10 +1,40 @@
-package com.T2IntroES;
+//package com.T2IntroES;
 
 /**
- * Created by Octavio on 5/30/2017.
+ * Classe que implementa a lógica do jogo Blackjack
+ * @author Guilherme Munaretto
+ * @author Octavio Carpez
  */
 public class Blackjack {
-    // onde a magica acontece
-    // faz o jogo acontecer
+
+    private Jogador jogador;
+    private Baralho baralho;
+    private Banca banca;
+
+    /**
+     * Método construtor da classe Blackjack
+     */
+    public Blackjack(){
+        this.jogador = new Jogador();
+        this.baralho = new Baralho();
+        this.banca = new Banca(baralho);
+    }
+
+    /**
+     * Retorna uma referência para o jogador da partida
+     * @return Uma referência para o jogador da partida
+     */
+    public Jogador getJogador(){
+        return this.jogador;
+    }
+
+    /**
+     * Retorna uma referência para o dealer da partida
+     * @return Uma referência para o dealer da partida
+     */
+    public Banca getBanca(){
+        return this.banca;
+    }
+
 
 }
