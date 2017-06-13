d557d2a7453b30ba45a41e09a79c9bd966365bb2
Guilherme <munaretto.guilherme@gmail.com>
2017-06-13 19:26:06 -0300

    Alterado o construtor da classe para receber também um valor numérico para a carta.

--- src/Carta.java
+++ src/Carta.java
@@ -1,30 +1,49 @@
-package com.T2IntroES;
+//package com.T2IntroES;
 
 /**
- * Created by Octavio on 5/30/2017.
+ * Classe responsável por armazenar as informações pertinentes à carta
+ * @author Guilherme Munaretto
+ * @author Octavio Carpez
  */
 public class Carta {
     private String naipe;
     private String valor;
+    private int valorNumerico;
 
-    public Carta(String naipe, String valor) {
+    public Carta(String naipe, String valor, int valorNumerico) {
         this.naipe = naipe;
         this.valor = valor;
+        this.valorNumerico = valorNumerico;
     }
 
+    /**
+     * Retorna uma String contendo o naipe da carta
+     * @return Uma String contendo o naipe da carta
+     */
     public String getNaipe() {
         return naipe;
     }
 
+    /**
+     * Retorna uma String contendo o valor da carta
+     * @return Uma String contendo o valor da carta
+     */
     public String getValor() {
         return valor;
     }
 
-    @Override
-    public String toString() {
-        return "Carta{" +
-                "naipe='" + naipe + '\'' +
-                ", valor='" + valor + '\'' +
-                '}';
+    /**
+     * Retorna um inteiro contendo o valor numérico da carta
+     * @return Um inteiro contendo o valor numérico da carta
+     */
+    public int getValorNumerico(){ return valorNumerico; }
+
+    /**
+     * Atualiza o valor numérico de uma carta de acordo com o inteiro passado por parâmetro
+     * @param valor O valor pelo qual deverá ser atualizado o valor numérico da carta
+     */
+    public void setValorNumerico(int valor){
+        this.valorNumerico = valor;
     }
+
 }
